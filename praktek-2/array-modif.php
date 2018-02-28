8e33a619cc06959c73329a21f01936470d39a912
samsulamin <samsul.rizqiamin@gmail.com>
2018-02-28 16:33:50 +0700

    menambah file array-modif.php

---
+++ praktek-2/array-modif.php
@@ -0,0 +1,13 @@
+<?php
+$data =array(
+	"nama" => "samsul",
+	"usis"=> 30,
+	"jomblo" = true,
+	0.1 => 5
+	);
+	
+echo "NAMA : " .$data["nama"] .<br>;
+echo "USIA : " .$data["usia"] .<br>;
+echo "STATUS JOMBLO : " .$data["jomblo"] .<br>;
+echo "DATA LAIN  : " .$data["0.1"] ;
+?>
