426c3f2742f7100c24918f0398538440705d185f
samsulamin <samsul.rizqiamin@gmail.com>
2018-02-28 17:40:37 +0700

    praktek 2

---
+++ praktek-2/input.php
@@ -0,0 +1,5 @@
+<form action = "proses.php" method ="post">
+NAMA : <input type="text" name="nama"> <br> 
+KELAS : <input type="text" name="kelas"> <br> 
+<input type="submit" value="proses">
+</form>
---
+++ praktek-2/proses.php
@@ -0,0 +1,5 @@
+<h2> selamat datang, <?php echo $_POST["nama"];?></h2>
+<br>
+<hr>
+pada semester ini, anda masuk ke kelas
+<?php echo $_POST["kelas"]; ?>
